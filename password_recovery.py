df461ea8f82150630a82efc01c9fefc933f12a3a
Markus Danek <markus.danek@gmail.com>
2016-05-11 11:53:41 +0200

    added binary to string func

--- password_recovery.py
+++ password_recovery.py
@@ -9,7 +9,6 @@ import pprint
 import string
 import numpy as np
 
-
 def str2bin(text, max_length=30):
     if len(text) > max_length:
         raise ValueError("Text can't contain more than {} symbols".format(max_length))
@@ -24,3 +23,15 @@ def str2bin(text, max_length=30):
         bits_list.extend(map(int, bits))
 
     return list(bits_list)
+
+def chunker(sequence, size):
+    for position in range(0, len(sequence), size):
+        yield sequence[position:position + size]
+
+def bin2str(array):
+    characters = []
+    for binary_symbol_code in chunker(array, size=8):
+        binary_symbol_str = ''.join(map(str, binary_symbol_code))
+        character = chr(int(binary_symbol_str, base=2))
+        characters.append(character)
+    return ''.join(characters).lstrip()
